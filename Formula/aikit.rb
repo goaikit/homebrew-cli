8a12dc14d98b35bdde4b13e06ce427351e340178
System Two <alexandre@55seeds.com>
2025-12-15 14:41:07 +0000

    Update aikit to version 0.1.4

--- Formula/aikit.rb
+++ Formula/aikit.rb
@@ -4,17 +4,27 @@
 class Aikit < Formula
   desc "Universal Package Manager for AI Agent Extensions"
   homepage "https://github.com/goaikit/aikit"
-  version "0.0.0"
+  version "0.1.4"
   license "MIT"
-
-  # Placeholder formula - will be updated when releases are available
-  # Run ./generate_formula.sh --version latest to update
 
   on_linux do
     if Hardware::CPU.intel?
-      # Formula will be generated automatically when releases are available
-      url "https://github.com/goaikit/aikit/releases/download/v0.0.0/placeholder"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      # Detect glibc version to choose appropriate binary
+      # glibc >= 2.38: use gnu binary for full features
+      # glibc < 2.38 or musl-based (Alpine): use musl binary for compatibility
+      glibc_version = begin
+        `ldd --version 2>&1`.lines.first.to_s[/(\d+\.\d+)/].to_f
+      rescue
+        0
+      end
+
+      if glibc_version >= 2.38
+        url "https://github.com/goaikit/aikit/releases/download/v0.1.4/aikit-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "ae1e9202bd2b45c37065658fd4de9a2068dcbd501d20688befe596a1cb2bc465"
+      else
+        url "https://github.com/goaikit/aikit/releases/download/v0.1.4/aikit-x86_64-unknown-linux-musl.tar.gz"
+        sha256 "d14e7fa257fae19778defdb0a13bb5965866126b10cff6742834496ad21780ad"
+      end
     end
   end
 
@@ -26,4 +36,3 @@ class Aikit < Formula
     system "#{bin}/aikit", "--version"
   end
 end
-
